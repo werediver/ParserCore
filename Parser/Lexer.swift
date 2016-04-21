9935cc35b47655a4ad5b2cec6b9e404d48385d65
Roman Fedoseev <werediver@gmail.com>
2016-04-21 18:34:23 +0300

    `Lexer` error reporting has been improved.

--- Parser/Lexer.swift
+++ Parser/Lexer.swift
@@ -1,15 +1,15 @@
 import Foundation
 
-enum LexerError<TargetSymbol: TerminalSymbolType>: ErrorType {
+struct UnknownTokenError<TargetSymbol: TerminalSymbolType>: ErrorType {
 
-    case UnknownToken(TargetSymbol.Source.Index)
+    let range: Range<TargetSymbol.Source.Index>
 
 }
 
-class Lexer<TargetSymbol: TerminalSymbolType>: SequenceType {
+final class Lexer<TargetSymbol: TerminalSymbolType>: SequenceType {
 
     typealias Token = GenericToken<TargetSymbol, TargetSymbol.Source.Index>
-    typealias Element = Result<Token, LexerError<TargetSymbol>>
+    typealias Element = Result<Token, UnknownTokenError<TargetSymbol>>
     typealias Generator = AnyGenerator<Element>
 
     let src: TargetSymbol.Source
@@ -18,30 +18,53 @@ class Lexer<TargetSymbol: TerminalSymbolType>: SequenceType {
         self.src = src
     }
 
+    private func findToken(offset: TargetSymbol.Source.Index) -> Token? {
+        var token: Token?
+        let rest = src.suffixFrom(offset)
+        if  rest.count > 0 {
+            for sym in TargetSymbol.all {
+                let len = sym.match(rest)
+                if len > 0 {
+                    token = Token(sym: sym, start: offset, end: offset.advancedBy(len))
+                    break
+                }
+            }
+        }
+        return token
+    }
+
+    func analyse(offset: TargetSymbol.Source.Index) -> Element? {
+        if src.fullRange.contains(offset) {
+            if let token = findToken(offset) {
+                return .Value(token)
+            } else {
+                let unknownTokenStart = offset
+                var unknownTokenEnd = offset.advancedBy(1)
+                while src.fullRange.contains(unknownTokenEnd)
+                   && findToken(unknownTokenEnd) == nil
+                {
+                    unknownTokenEnd = unknownTokenEnd.advancedBy(1)
+                }
+                return .Error(UnknownTokenError(range: unknownTokenStart ..< unknownTokenEnd))
+            }
+        } else {
+            return nil
+        }
+    }
+
     func generate() -> Generator {
         let src = self.src
         var offset = src.startIndex
-        var failed = false
         return AnyGenerator {
-            var token: Token?
-            let rest = src.suffixFrom(offset)
-            if rest.count > 0 && !failed {
-                for sym in TargetSymbol.all {
-                    let len = sym.match(rest)
-                    if len > 0 {
-                        let tokenEnd = offset.advancedBy(len)
-                        token = Token(sym: sym, start: offset, end: tokenEnd)
-                        offset = tokenEnd
-                        break
-                    }
-                }
-                failed = (token == nil)
-                return token.flatMap{ .Value($0) } ?? .Error(.UnknownToken(offset))
-            } else {
-                // It's possible to continue lexical analysis bypassing errors, but is it useful?
-                // TODO: Implement ability to optionally bypass errors in Lexer.
-                return nil // Stop iteration.
+            let result = self.analyse(offset)
+
+            if let result = result,
+               let nextOffset = result.value?.range.endIndex ?? result.error?.range.endIndex
+            {
+                offset = nextOffset
             }
+
+            return result
         }
     }
 
